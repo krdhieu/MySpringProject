c9e15ac4c9171e916482d6136f4f1b8e6731fcd5
Nguyen Hieu <nguyentrunghieuhp20@gmail.com>
2023-05-25 16:59:23 +0700

    done security and jwt configuation

---
+++ src/main/java/com/app/config/ApplicationConfig.java
@@ -0,0 +1,53 @@
+package com.app.config;
+
+import com.app.config.security.MyUserDetails;
+import com.app.logic.AccountLogic;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Autowired
+            private AccountLogic accountLogic;
+
+            @Override
+            public MyUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return new MyUserDetails().setAccount(accountLogic.findAccountByUsername(username));
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+}
--- src/main/java/com/app/config/security/MyUserDetails.java
+++ src/main/java/com/app/config/security/MyUserDetails.java
@@ -7,25 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 
-public class MyUserDetail implements UserDetails {
+public class MyUserDetails implements UserDetails {
     Account account;
-
     @Autowired
     AccountRolePermissionLogic accountRolePermissionLogic;
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<AccountRolePermission> accountRolePermissions = accountRolePermissionLogic.findRolePermissionByAccountId(account.getId());
         List<GrantedAuthority> authorities = new ArrayList<>();
-        if(accountRolePermissions.isEmpty()) return authorities;
+        if (accountRolePermissions.isEmpty()) return authorities;
         authorities.add(new SimpleGrantedAuthority(accountRolePermissions.get(0).getRole().getName()));
         accountRolePermissions.stream().map(accountRolePermission -> {
             String permissionName = accountRolePermission.getPermission().getName();
@@ -36,31 +32,36 @@ public class MyUserDetail implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return account.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return account.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
+    }
+
+    public MyUserDetails setAccount(Account account) {
+        this.account = account;
+        return this;
     }
 }
