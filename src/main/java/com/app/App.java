804a2526ff058e56d7e1662b7d53f0e0b7970866
Nguyen Hieu <nguyentrunghieuhp20@gmail.com>
2023-06-11 08:38:21 +0700

    deploy app to aws ec2 ubuntu tomcat

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -2,13 +2,20 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
-public class App {
+public class App  extends SpringBootServletInitializer {
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         ApplicationContext context = SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
     }
 
 }
